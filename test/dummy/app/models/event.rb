00e91bf8c6a4179255f31ed5cac127b175030e7e
Rafael Nowrotek <mail@benignware.com>
2015-11-18 19:35:36 +0100

    Fixed default datetime not defined but used by form in create view

--- test/dummy/app/models/event.rb
+++ test/dummy/app/models/event.rb
@@ -2,8 +2,9 @@ class Event < ActiveRecord::Base
   after_initialize :init_event
   
   def init_event
-    self.date||= Date.tomorrow
-    self.time||= Time.now + 1.hour
+    self.date||= Date.today
+    self.datetime||= (DateTime.now + 1.hour).change(min: 0, sec: 0)
+    self.time||= (Time.current + 1.hour).change(min: 0, sec: 0)
   end
   
 end
