f4bcabfa153f65f48c19c0642a812b424586f3b9
Rafael Nowrotek <mail@benignware.com>
2016-10-30 21:22:34 +0100

    Fix data_format submit, handle min and max date

--- lib/date_picker/styles/flatpickr.rb
+++ lib/date_picker/styles/flatpickr.rb
@@ -4,22 +4,27 @@ module DatePicker
       def types
         [:date, :datetime, :time]
       end
-      def mapping()
+      def mapping
         :flatpickr
       end
-      def template() 
+      def template
         %{
           <input id="<%= input_id %>" name="<%= name %>"/>
           <script>
             (function() {
               var
                 picker = flatpickr && flatpickr('#<%= input_id %>', {
-                  dateFormat: "<%= picker_format %>",
+                  dateFormat: "<%= data_format %>",
                   timeFormat: '\u2063',
                   enableTime: <%= type.to_s != 'date' %>,
                   noCalendar: <%= type.to_s == 'time' %>,
                   utc: <%= type.to_s == 'time' %>,
-                  defaultDate: new Date(<%= time %>)
+                  defaultDate: new Date(<%= time %>),
+                  minDate: <%= min ? 'new Date("' + min.to_s + '")' : 'undefined' %>,
+                  maxDate: <%= max ? 'new Date("' + max.to_s + '")' : 'undefined' %>,
+                  altInput: true,
+                  altFormat: "<%= picker_format %>",
+                  time_24hr: <%= /(?<!\\\\\\\\)H/ === picker_format %>
                 });
               if (picker) {
                 //picker.setDate(new Date(<%= time %>));
