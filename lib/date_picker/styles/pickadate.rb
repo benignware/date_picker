a39e69fa1aa779a16a8a3fe32840b8c44bbe438d
Rafael Nowrotek <mail@benignware.com>
2016-10-30 18:54:07 +0100

    Remove debug output

--- lib/date_picker/styles/pickadate.rb
+++ lib/date_picker/styles/pickadate.rb
@@ -10,10 +10,8 @@ module DatePicker
       def template() 
         %{
           <%= input_html %>
-          <!--<input id="<%= input_id %>_hidden" type="hidden" value="<%= value %>" name="<%= name %>"/>-->
           <script>
             (function() {
-              console.log("-->", '#<%= input_id %>');
               var
                 type = '<%= type %>',
                 plugin = 'picka' + type,
@@ -24,21 +22,10 @@ module DatePicker
                   weekdaysShort: <%= abbr_day_names.to_json %>,
                   format: '<%= picker_format %>',
                   formatSubmit: '<%= data_format %>',
-                  hiddenName: true,
-                  onSet: function(e) {
-                    console.log("****SET");
-                  }
+                  hiddenName: true
                 }),
                 $element = $('#<%= input_id %>'),
                 picker = $element[plugin] && $element[plugin](options)[plugin]('picker');
-              
-              console.log('--->', plugin, picker.set, picker, $element[plugin]);
-              
-              if (picker) { 
-                console.log("SET", <%= time %>);
-                //picker.set('select', <%= time %>);
-              }
-              
             })();
           </script>
         }
